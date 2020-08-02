b20e2383a6e7429a3799ad37ca31087333a5aa9e
gnkwon95 <gnkwon95@gmail.com>
2020-08-02 12:54:33 +0900

    modify backend admin

--- backend/proj/admin.py
+++ backend/proj/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.apps import apps
 
 # Register your models here.
-from .models import MentorProfile, PersonalProfile, Score, Connections
+from .models import MentorProfile, PersonalProfile, Score, Connections, MentorProfileWorkExperience, MentorProfileExtracurricular, MentorProfileCertificates, MentorProfileAppliedCompanies
 
 class MentorProfileAdmin(admin.ModelAdmin):
     search_fields = ['title']
@@ -14,3 +14,8 @@ admin.site.register(MentorProfile, MentorProfileAdmin)
 admin.site.register(PersonalProfile, PersonalProfileAdmin)
 admin.site.register(Score)
 admin.site.register(Connections)
+
+admin.site.register(MentorProfileWorkExperience)
+admin.site.register(MentorProfileExtracurricular)
+admin.site.register(MentorProfileCertificates)
+admin.site.register(MentorProfileAppliedCompanies)
