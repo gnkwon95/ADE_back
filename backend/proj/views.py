d495ad948d55da8872357f57ec0804172572153b
gnkwon95 <gnkwon95@gmail.com>
2020-08-07 13:29:43 +0900

    connect

--- backend/proj/views.py
+++ backend/proj/views.py
@@ -47,7 +47,9 @@ class ConnectionsViewSet(LoggingMixin, viewsets.ModelViewSet):
         if mentee is not None:
             queryset = queryset.filter(mentee = mentee)
         if user is not None:
-            queryset = queryset.filter(mentor__user__user_uid = user) #or mentee. filter on two fields.
+            queryset1 = queryset.filter(mentor__user__user_uid = user) #or mentee. filter on two fields.
+            queryset2 = queryset.filter(mentee__uid = user)
+            return queryset1 | queryset2
         return queryset
 
 
